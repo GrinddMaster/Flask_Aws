14e58020649f0e0511914af616ced0d2554b330a
GrinddMaster <ali.hussein15321@gmail.com>
2025-05-19 20:10:06 +0300

    Now the sign up page can redirect after the user is added next in the form I should add at the action to send to an endpoint that connects to the database to add the user to the database

--- CyberApp/app.py
+++ CyberApp/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 def Create_app():
     app = Flask(__name__)
+    app.secret_key = " Test key"
     from CyberApp.blueprints.Login.routes import login
     from CyberApp.blueprints.Core.routes import core
     from CyberApp.blueprints.Create_User.routes import signup
--- CyberApp/blueprints/Create_User/routes.py
+++ CyberApp/blueprints/Create_User/routes.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
-
+from flask import render_template, Blueprint, request, flash, redirect, url_for
 
 signup = Blueprint('signup', __name__, template_folder='templates')
+signup.secret_key = ' Hello World '
 
 
-@signup.route('/Sign_up', methods=['GET'])
+@signup.route('/Sign_up', methods=['GET', 'POST'])
 def Sign_up():
-    return render_template('Sign_up.html')
+    if request.method == 'POST':
+        flash('Sign up Successful !')
+        return redirect(url_for('login.login_page'))
+    elif request.method == 'GET':
+        return render_template('Sign_up.html')
