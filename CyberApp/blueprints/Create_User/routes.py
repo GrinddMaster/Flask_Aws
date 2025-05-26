d48686d52a80c320f0d0c6511b9fb52ba5aa25b1
GrinddMaster <ali.hussein15321@gmail.com>
2025-05-26 21:28:31 +0300

    The sign up page is now working and updates the database

--- CyberApp/blueprints/Create_User/routes.py
+++ CyberApp/blueprints/Create_User/routes.py
@@ -1,4 +1,6 @@
 from flask import render_template, Blueprint, request, flash, redirect, url_for
+from CyberApp.blueprints.User.models import User
+from CyberApp.app import db
 
 signup = Blueprint('signup', __name__, template_folder='templates')
 signup.secret_key = ' Hello World '
@@ -8,6 +10,17 @@ signup.secret_key = ' Hello World '
 def Sign_up():
     if request.method == 'POST':
         flash('Sign up Successful !')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        regist_user = User.query.filter_by(Name=username).first()
+        if regist_user:
+            flash("User already exists ! ")
+            return redirect(url_for(signup.Sign_up))
+        new_user = User(Name=username, Password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
         return redirect(url_for('login.login_page'))
     elif request.method == 'GET':
         return render_template('Sign_up.html')
