3337fa45af115f9458efcfad6749afbcd5fda9c6
GrinddMaster <ali.hussein15321@gmail.com>
2025-05-27 00:00:24 +0300

    Added a Signup Href and an email column to the database

--- CyberApp/blueprints/Create_User/routes.py
+++ CyberApp/blueprints/Create_User/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, Blueprint, request, flash, redirect, url_for
 from CyberApp.blueprints.User.models import User
-from CyberApp.blueprints.User.routes import disp_user
 from CyberApp.app import db
 
 signup = Blueprint('signup', __name__, template_folder='templates')
@@ -13,12 +12,13 @@ def Sign_up():
         flash('Sign up Successful !')
         username = request.form.get('username')
         password = request.form.get('password')
+        email = request.form.get('email')
         regist_user = User.query.filter_by(Name=username).first()
 
         if regist_user:
             flash("User already exists ! ")
             return redirect(url_for(signup.Sign_up))
-        new_user = User(Name=username, Password=password)
+        new_user = User(Name=username, Password=password, Email=email)
         db.session.add(new_user)
         db.session.commit()
 
